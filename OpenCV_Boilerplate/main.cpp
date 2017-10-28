9cd23dd3ff670b44588ce32df7a344c9a96b1bfc
ShihaoXu <konto.shxu@gmail.com>
2017-10-29 00:49:21 +0200

    Initial Commit

---
+++ OpenCV_Boilerplate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_Boilerplate
+//
+//  Created by Shihao Xu on 29.10.17.
+//  Copyright © 2017 Shihao Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
